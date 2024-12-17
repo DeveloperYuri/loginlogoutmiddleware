f6f5886ac5edd03b0124c299132ce9cbef2cfd1e
DeveloperYuri <developeryuri2@gmail.com>
2024-12-17 16:27:10 +0700

    updateforgotpasswordok

--- app/Mail/ForgotPasswordMail.php
+++ app/Mail/ForgotPasswordMail.php
@@ -44,7 +44,7 @@ class ForgotPasswordMail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.forgot_password',
         );
     }
 
